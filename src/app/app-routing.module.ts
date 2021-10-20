a6bad1a09a24c98264beb7370319c563976b1445
antkor <antkorsc@gmail.com>
2021-10-20 16:26:01 +0300

    added auth guard to all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,18 +18,22 @@ const routes: Routes = [
   {
     path: 'news-single',
     loadChildren: () => import('~/app/news-single/news-single.module').then((m) => m.NewsSingleModule),
+    canActivate: [AuthenticationGuard]
   },
   {
     path: 'news',
     loadChildren: () => import('~/app/news/news.module').then((m) => m.NewsModule),
+    canActivate: [AuthenticationGuard]
   },
   {
     path: 'stores',
     loadChildren: () => import('~/app/stores/stores.module').then((m) => m.StoresModule),
+    canActivate: [AuthenticationGuard]
   },
   {
     path: 'about',
     loadChildren: () => import('~/app/about/about.module').then((m) => m.AboutModule),
+    canActivate: [AuthenticationGuard]
   },
 ]
 
